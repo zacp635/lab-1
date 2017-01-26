f46c55de78ed3d9ef129ab6de41cc734040f29fa
Zachary Morgan <zmorgan@knuth.cs.nmsu.edu>
2017-01-26 12:46:39 -0700

    Added and updated .gitignore and helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+//Zachary Morgan
+//1/26/17
+//CS371
+//Lab1
+
+public class helloworld{
+	public static void main (String args[]){
+		System.out.println("H3ll0 W0rld......");
+
+	}//end main
+}//end class
